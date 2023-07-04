d3264b38f3ab6943b570b07117b4a6d99a79d65f
Roshan <roshan.jha@hih7.in>
2023-07-04 11:26:28 +0530

    Start GraphQL

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`SERVER IS RUNNING ON ${PORT}`);
