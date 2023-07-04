936b7eb9b3c1a57149d363424b853145aa636cb2
Roshan <roshan.jha@hih7.in>
2023-07-04 12:00:13 +0530

    Client Type & Queries

--- schema/schema.js
+++ schema/schema.js
@@ -1,3 +1,38 @@
 const { projects, clients } = require("../sampleData");
 
-const {} = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLSchema,
+} = require("graphql");
+
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    clients: {
+      type: ClientType,
+      args: {
+        id: {
+          type: GraphQLID,
+        },
+      },
+      resolve(parentValue, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
